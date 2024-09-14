540b3307bba4dd58c51530613f10c4683494f366
Hafsa Abid <128362965+hafsaabidsaeed@users.noreply.github.com>
2024-09-14 23:56:31 +0500

    added cloudinary package

---
+++ Backend/configurations/cloudinary.js
@@ -0,0 +1,15 @@
+const cloudinary = require('cloudinary').v2;
+
+// Configure your Cloudinary API credentials
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+module.exports = cloudinary;
+
+
+
+
+  
