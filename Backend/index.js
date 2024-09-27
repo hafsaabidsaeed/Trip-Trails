a12a2346aaaa67041037aeac0e7db38b7a98ded1
Hafsa Abid <128362965+hafsaabidsaeed@users.noreply.github.com>
2024-09-27 11:25:20 +0500

    index.js
    Added CORS configurations in index.js file

--- Backend/index.js
+++ Backend/index.js
@@ -1,44 +1,25 @@
 const express = require('express');
 const connectToDb = require('./configurations/db');
 require('dotenv').config();
-const app = express();
+const path = require('path'); // Import path module
+const cors = require('cors');
 const cityRoutes = require('./routes/placesRoutes');
 const tourPackageRoutes = require('./routes/tourPackageRoutes.js');
-const cors = require('cors');
-const path = require('path'); // Import path module
-const upload = require('./configurations/multer'); // Import the multer configuration
 
-app.post('/upload', upload, async (req, res) => {
-    if (!req.file) {
-      return res.status(400).send('No file uploaded');
-    }
-  
-    // Save the image URL in MongoDB
-    const newImage = new ImageModel({
-      url: req.file.path, // Cloudinary image URL
-      public_id: req.file.filename, // Cloudinary public ID
-    });
-  
-    try {
-      await newImage.save();
-      res.status(200).send('Image uploaded to Cloudinary and saved to MongoDB');
-    } catch (error) {
-      res.status(500).send('Error saving image to MongoDB');
-    }
-  });
+const app = express();
 
- 
 // Enable CORS for all routes
 app.use(cors());
 
 // Connect Database
 connectToDb(); 
 
+// Serve static files from the "uploads" directory for images
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+
 // Init Middleware
 app.use(express.json());
-
-// Serve static files from the uploads directory
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Define Routes
 app.use('/api/auth', require('./routes/userRoutes'));
